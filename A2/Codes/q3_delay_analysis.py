85f15c6c16162cf3202885bd59506badf2ccacf0
techoflashh <hy815088@gmail.com>
2023-10-29 14:28:30 +0530

    A2 q3 q4 q5 done

--- A2/Codes/q3_delay_analysis.py
+++ A2/Codes/q3_delay_analysis.py
@@ -2,29 +2,29 @@ import os
 import subprocess
 import time
 
-fp3 = open("output_delay.txt",'w')
+fp3 = open("q3_delay_output.txt",'w')
 fp3.close()
 
 command = "ngspice tempfile.cir"
 
-for j in range(0,8):
+temperatures = []
 
-    if j<4:
+with open('temperatureValues.txt') as f:
+    temperatures = f.readlines()
+    temperatures = [float(i[:-1]) for i in temperatures]
+
+temperatures = temperatures[::10]
+
+for t in temperatures:
+    search_text_temp = "*temperature"
+    fp3 = open("q3_delay_output.txt",'a')
+    replace_text_temp = f".option temp={t}\n"
+    fp3.write(f"\nTemperature = {t}\n")
+    for j in range(0,3):
         s1 = "node_a"+str(j)
-    else:
-        s1 = "node_b"+str(j-4)
-
-    for i in range(0,4):
-
-        s2 = "node_out"+str(i) 
-
-        if(j<4):
-            fp1 = open("delay1.cir",'r')
-        else:
-            fp1 = open("delay2.cir",'r')
-
-        fp2 =open("tempfile.cir",'w')
-        fp3 = open("output_delay.txt",'a')
+        s2 = "node_out"
+        fp1 = open("q3_delay.cir","r")
+        fp2 = open("tempfile.cir",'w')
         mode1 = "RISE"
         mode2 = "RISE"
         mode3 = "FALL"
@@ -32,20 +32,20 @@ for j in range(0,8):
 
         data = fp1.read()
 
-        search_text = "*target text"
+        search_text = "*delay"
         replace_text = f'''
 .measure tran trise 
-+ TRIG v({s1}) VAL = 'SUPPLY/2' {mode1} =1
-+ TARG v({s2}) VAL = 'SUPPLY/2' {mode2} =1 
++ TRIG v({s1}) VAL = 'SUPPLY/2' {mode1} = 1
++ TARG v({s2}) VAL = 'SUPPLY/2' {mode2} = 1 
 
 .measure tran tfall 
-+ TRIG v({s1}) VAL = 'SUPPLY/2' {mode3} =1 
-+ TARG v({s2}) VAL = 'SUPPLY/2' {mode4}=1
++ TRIG v({s1}) VAL = 'SUPPLY/2' {mode3} = 1 
++ TARG v({s2}) VAL = 'SUPPLY/2' {mode4} = 1
 
 .measure tran tpd param = '(trise + tfall)/2' goal = 0
         '''
-
         data = data.replace(search_text,replace_text)
+        data = data.replace(search_text_temp,replace_text_temp)
         fp2.write(data)
         fp1.close()
         fp2.close()
@@ -54,7 +54,7 @@ for j in range(0,8):
         if completed_process.returncode == 0:
             output = completed_process.stdout
         else:
-            output = ("Command execution failed. at",str(i),str(j))
+            output = ("Command execution failed. at",str(j))
 
         output = output.split('\n')
         output = output[-4] 
@@ -62,4 +62,5 @@ for j in range(0,8):
 
         fp3.write(output+additional_text)
         # time.sleep(0.4)
+    fp3.close()
 subprocess.run(["rm","tempfile.cir"])
